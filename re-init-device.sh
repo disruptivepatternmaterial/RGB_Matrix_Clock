6de1d1ac992701b1fa8e7f81adfb92d91aec7e9a
ntableman <nathan@tableman.com>
2022-06-05 13:32:44 -0700

    fixing fonts

---
+++ re-init-device.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+rm -r /Volumes/CIRCUITPY
+cp -r  ~/Documents/GitHub/kitchen_clock/lib  /Volumes/CIRCUITPY/lib
+cp -r  ~/Documents/GitHub/kitchen_clock/bmps  /Volumes/CIRCUITPY/bmps
+cp ~/Documents/GitHub/kitchen_clock/6x12.bdf /Volumes/CIRCUITPY/
+cp ~/Documents/GitHub/kitchen_clock/boot.py /Volumes/CIRCUITPY/
+cp ~/Documents/GitHub/kitchen_clock/clock26.bdf /Volumes/CIRCUITPY/
+cp ~/Documents/GitHub/kitchen_clock/code.py /Volumes/CIRCUITPY/
+cp ~/Documents/GitHub/kitchen_clock/kitchen_clock.py /Volumes/CIRCUITPY/
+cp ~/Documents/GitHub/kitchen_clock/secrets.py /Volumes/CIRCUITPY/
